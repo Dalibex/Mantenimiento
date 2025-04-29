14455cad169db1da787ce6dea64300337d61ac35
Lewas88 <Lemaprieto@uma.es>
2025-04-29 16:24:54 +0200

    up

--- Individual/src/main/java/purchase/Warehouse.java
+++
@@ -1,9 +0,0 @@
-package purchase;
-
-public interface Warehouse {
-  public boolean contains(String name);
-  public boolean thereAreProducts(String name, int amount);
-  
-  public void remove(String name, int amount);
-  public int getAmount(String name);
-}
